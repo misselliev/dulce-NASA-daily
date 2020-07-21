8b299f7dec4f4c5f83644d80d18b3efd0a82c9d5
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-21 15:38:35 -0700

    Updating favoritesReducer

--- src/Redux/favoritesReducer.js
+++ src/Redux/favoritesReducer.js
@@ -1,7 +1,9 @@
 export default (state = {}, { type, payload }) => {
   switch (type) {
-    case 'SET FAVORITES':
+    case 'LOAD_FAVORITES':
       return { ...state, favorites: payload };
+    case 'ADD_FAVORITE':
+      return { ...state, favorites: [...state.favorites, payload] };
     default:
       return state;
   }
