33d5f19d7b085a61efada8eb27a5ecce0adb255a
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-07-21 15:40:01 -0700

    Updating favicon, updating styling and Home header

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ import ImageCard from '../Components/ImageCard';
 
 const Home = () => (
   <Container>
-    <Header as="h1">HomePage</Header>
+    <Header as="h1" className="main-header">The sky today looks like this:</Header>
     <ImageCard />
   </Container>
 );
