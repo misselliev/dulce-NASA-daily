53982f87a521b4392fa696adf82ccbfdba0d622f
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-08 11:33:26 -0700

    Updating firebaseConfig to include correct database import

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,16 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: 'https://dulce-nasa-daily.firebaseio.com',
-  projectId: 'dulce-nasa-daily',
-  storageBucket: 'dulce-nasa-daily.appspot.com',
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: '1:1034197701360:web:62237b59cd0e4a4be16835',
-  measurementId: 'G-PKQHPPQ36P',
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURAMENT_ID,
 };
-  // Initialize Firebase
 const firebaseConfig = firebase.initializeApp(config);
-firebase.analytics();
 
 export default firebaseConfig;
