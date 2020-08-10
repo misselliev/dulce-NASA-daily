1f0ea5b6c22f4fbb3cf3a25396da216fa18ced8d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-08-10 16:20:00 -0700

    Updating with TypeScript: Favorites, Home and index page and ImageCard, Navbar components

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,24 +3,24 @@ import { Link } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
 const Navbar: React.FC = () => (
-  <nav className="ui container">
-    <Menu>
-      <Link className="header item" to="/">
-        <i className="rocket icon" />
-        Dulce NASA
-      </Link>
-      <Menu.Item className="menu right">
-        <Link className="item" href="/" to="/">
-          <i className="home icon" />
-          Home
-        </Link>
-        <Link className="item" href="/favorites" to="/favorites">
-          <i className="heart icon" />
-          Favorites
-        </Link>
-      </Menu.Item>
-    </Menu>
-  </nav>
+    <nav className="ui container">
+        <Menu>
+            <Link className="header item" to="/">
+                <i className="rocket icon" />
+                Dulce NASA
+            </Link>
+            <Menu.Item className="menu right">
+                <Link className="item" href="/" to="/">
+                    <i className="home icon" />
+                    Home
+                </Link>
+                <Link className="item" href="/favorites" to="/favorites">
+                    <i className="heart icon" />
+                    Favorites
+                </Link>
+            </Menu.Item>
+        </Menu>
+    </nav>
 );
 
 export default Navbar;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+import ImageCard from '../Components/ImageCard';
+
+const Home: React.FC = () => (
+    <Container>
+        <Header as="h1" className="main-header">
+            The sky today looks like this:
+        </Header>
+        <ImageCard />
+    </Container>
+);
+export default Home;
